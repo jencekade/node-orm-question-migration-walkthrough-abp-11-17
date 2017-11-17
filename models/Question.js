f352dafc22359046fc9dd059c03b22266fbca9d3
jencekade <gaephuoc@gmail.com>
2017-11-17 04:36:11 +0000

    automatically backed up by learn

--- models/Question.js
+++ models/Question.js
@@ -8,7 +8,7 @@ class Question {
       const sql =
         `CREATE TABLE questions (
           id INTEGER PRIMARY KEY,
-          context TEXT
+          content TEXT
         )`;
 
       db.run(sql, function() {
