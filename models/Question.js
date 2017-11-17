f36aa40cf1be1e4e175b13993a4b9aae381fb222
jencekade <gaephuoc@gmail.com>
2017-11-17 04:38:24 +0000

    automatically backed up by learn

--- models/Question.js
+++ models/Question.js
@@ -6,10 +6,10 @@ class Question {
   static CreateTable() {
 
     const sql =
-      CREATE TABLE questions (
+      `CREATE TABLE questions (
           id INTEGER PRIMARY KEY,
           content TEXT
-        );
+        )`;
     return new Promise(function(resolve) {
       db.run(sql, function() {
         resolve("Success");
